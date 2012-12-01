595e7d466347eedcd5b00bccc219a9d67a6d8c41
Daniel Egeberg <daniel.egeberg@gmail.com>
2012-12-01 17:22:55 +0100

    Some code. May or may not work. Not tested.

--- src/assignmentImplementation/KeyValueBaseImpl.java
+++ src/assignmentImplementation/KeyValueBaseImpl.java
@@ -1,5 +1,9 @@
 package assignmentImplementation;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
@@ -14,63 +18,109 @@ import keyValueBaseInterfaces.KeyValueBase;
 import keyValueBaseInterfaces.Pair;
 import keyValueBaseInterfaces.Predicate;
 
-public class KeyValueBaseImpl implements KeyValueBase<KeyImpl,ValueListImpl>
-{
+public class KeyValueBaseImpl implements KeyValueBase<KeyImpl, ValueListImpl> {
+    private boolean initialized;
+    private IndexImpl index;
 
     public KeyValueBaseImpl(IndexImpl index) {
-        // TODO Auto-generated constructor stub
+        initialized = false;
+        this.index = index;
     }
 
     @Override
     public void init(String serverFilename)
             throws ServiceAlreadyInitializedException,
             ServiceInitializingException, FileNotFoundException {
-        // TODO Auto-generated method stub
-        
+        if (initialized) {
+            throw new ServiceAlreadyInitializedException();
+        }
+
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader(serverFilename)));
+
+            while (s.hasNextLine()) {
+                // TODO: Do stuff
+                System.out.println(s.next());
+            }
+        } catch (Exception e) {
+            throw new ServiceInitializingException("something is fucked up");
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+
+        initialized = true;
     }
 
     @Override
     public ValueListImpl read(KeyImpl k) throws KeyNotFoundException,
             IOException, ServiceNotInitializedException {
-        // TODO Auto-generated method stub
-        return null;
+        if (!initialized) {
+            throw new ServiceNotInitializedException();
+        }
+
+        return index.get(k);
     }
 
     @Override
     public void insert(KeyImpl k, ValueListImpl v)
             throws KeyAlreadyPresentException, IOException,
             ServiceNotInitializedException {
-        // TODO Auto-generated method stub
-        
+        if (!initialized) {
+            throw new ServiceNotInitializedException();
+        }
+
+        index.insert(k, v);
     }
 
     @Override
     public void update(KeyImpl k, ValueListImpl newV)
             throws KeyNotFoundException, IOException,
             ServiceNotInitializedException {
-        // TODO Auto-generated method stub
-        
+        if (!initialized) {
+            throw new ServiceNotInitializedException();
+        }
+
+        index.update(k, newV);
     }
 
     @Override
     public void delete(KeyImpl k) throws KeyNotFoundException,
             ServiceNotInitializedException {
-        // TODO Auto-generated method stub
-        
+        if (!initialized) {
+            throw new ServiceNotInitializedException();
+        }
+
+        index.remove(k);
     }
 
     @Override
-    public List<ValueListImpl> scan(KeyImpl begin, KeyImpl end, Predicate<ValueListImpl> p)
-            throws IOException, BeginGreaterThanEndException,
-            ServiceNotInitializedException {
-        // TODO Auto-generated method stub
-        return null;
+    public List<ValueListImpl> scan(KeyImpl begin, KeyImpl end,
+            Predicate<ValueListImpl> p) throws IOException,
+            BeginGreaterThanEndException, ServiceNotInitializedException {
+        if (!initialized) {
+            throw new ServiceNotInitializedException();
+        }
+
+        ArrayList<ValueListImpl> list = new ArrayList<ValueListImpl>();
+        for (ValueListImpl v : index.scan(begin, end)) {
+            if (p.evaluate(v)) {
+                list.add(v);
+            }
+        }
+        return list;
     }
 
     @Override
     public List<ValueListImpl> atomicScan(KeyImpl begin, KeyImpl end,
             Predicate<ValueListImpl> p) throws IOException,
             BeginGreaterThanEndException, ServiceNotInitializedException {
+        if (!initialized) {
+            throw new ServiceNotInitializedException();
+        }
         // TODO Auto-generated method stub
         return null;
     }
@@ -78,8 +128,11 @@ public class KeyValueBaseImpl implements KeyValueBase<KeyImpl,ValueListImpl>
     @Override
     public void bulkPut(List<Pair<KeyImpl, ValueListImpl>> mappings)
             throws IOException, ServiceNotInitializedException {
-        // TODO Auto-generated method stub
+        if (!initialized) {
+            throw new ServiceNotInitializedException();
+        }
         
+        index.bulkPut(mappings);
     }
 
 }
--- src/assignmentImplementation/StoreImpl.java
+++ src/assignmentImplementation/StoreImpl.java
@@ -1,20 +1,34 @@
 package assignmentImplementation;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+
+import keyValueBaseInterfaces.MemoryMappedFile;
 import keyValueBaseInterfaces.Store;
 
 public class StoreImpl implements Store
 {
+    final private static long TOTAL_SIZE = 10*1024*1024*1024; // 10 GB
+    
+    private RandomAccessFile raf;
+    private MemoryMappedFile mmapfile;
+    
+    public StoreImpl() throws IndexOutOfBoundsException, IOException {
+        raf = new RandomAccessFile("/tmp/pcsd_store", "rw");
+        mmapfile = new MemoryMappedFile(raf.getChannel(), FileChannel.MapMode.READ_WRITE, 0, TOTAL_SIZE);
+    }
 
     @Override
     public byte[] read(Long position, int length) {
-        // TODO Auto-generated method stub
-        return null;
+        byte[] res = new byte[length];
+        mmapfile.get(res, position);
+        return res;
     }
 
     @Override
     public void write(Long position, byte[] value) {
-        // TODO Auto-generated method stub
-        
+        mmapfile.put(value, position);
     }
     
 }
